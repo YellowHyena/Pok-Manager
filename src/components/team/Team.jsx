c05d9f958f74c1eed752e3657037e3164b593d3b
Max Ekholm <90773776+YellowHyena@users.noreply.github.com>
2022-04-22 12:27:36 +0200

    now it's done. changing names was not working lol

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -2,34 +2,18 @@ import './teamStyling/Team.css'
 const Team = ({ team, setTeam }) => {
 
 	//updates the nickname, duh
-	const updateNickname = (member, index) => e => {
+	const updateNickname = (member, index, e) => {
 		let newTeam = [...team]
 		newTeam[index] = { id: member.id, name: e.target.value }
 		setTeam(newTeam)
 	}
 
-	//renders 6 balls, checks what teammembers there are and renders them inside ball. if no member is left empty balls render
-	const renderBalls = () => {
+	//returns empty balls based on team size
+	const EmptyBalls = () => {
 		const balls = []
-		for (let i = 0; i < 6; i++) {
-			const member = team[i]
-			if (member) {
-				balls.push(
-					<li key={member.name} className='ball'>
-						<div className='middle' />
-						<div className='color' />
-						<div className='lid'>
-							<img className='sprite-shadow' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${member.id}.png`} alt="pokemon" />
-						</div>
-						<img className='sprite' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${member.id}.png`} alt="pokemon" />
-
-						<div className='edge' />
-						<input className='nickname' type='text' defaultValue={member.name} onChange={() => updateNickname(member, index) + console.log(member.name)} ></input>
-					</li>
-				)
-			}
-			else balls.push(
-				<li key={'empty-ball-'+i} className='ball'>
+		for (let i = 0; i <  6 - team.length; i++) {
+			balls.push(
+				<li key={'empty-ball-' + i} className='ball'>
 					<div className='middle' />
 					<div className='color' />
 					<div className='lid'></div>
@@ -37,14 +21,27 @@ const Team = ({ team, setTeam }) => {
 				</li>
 			)
 		}
+		console.log('empty balls =', balls.length );
 		return balls
 	}
 	return (
 		<section id='team-section'>
 			<ul className='team'>
-				{renderBalls()}
+				{team.map((member, index) =>
+					<li key={member.id} className='ball'>
+						<div className='middle' />
+						<div className='color' />
+						<div className='lid'>
+							<img className='sprite-shadow' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${member.id}.png`} alt="pokemon" />
+						</div>
+						<img className='sprite' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${member.id}.png`} alt="pokemon" />
+						<div className='edge' />
+						<input className='nickname' type='text' defaultValue={member.name} onChange={(e) => updateNickname(member, index, e) + console.log(member.name)} ></input>
+					</li>
+				)}
+				{EmptyBalls()}
 			</ul>
-		</section>
+		</section >
 	);
 }
 
